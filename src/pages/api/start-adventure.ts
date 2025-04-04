4697e1a374a06d4d76b273d9193d080737fafc97
Braden Godley <braden@bgodley.com>
2025-04-03 21:04:19 -0700

    begin the prompting

--- src/pages/api/start-adventure.ts
+++ src/pages/api/start-adventure.ts
@@ -1,3 +1,5 @@
+import { generateContent } from "@/utilities/completions";
+import { XMLParser } from "fast-xml-parser";
 import type { NextApiRequest, NextApiResponse } from "next";
 import z from "zod";
 
@@ -10,7 +12,7 @@ type ResponseData =
           error: string;
       };
 
-export default function handler(
+export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<ResponseData>,
 ) {
@@ -27,8 +29,59 @@ export default function handler(
         return res.status(400).json({ error: validation.error.message });
     }
 
+    const body = req.body as z.infer<typeof schema>;
+
+    const prompt = generatePrompt(body.characterName);
+
+    const generatedContent = await generateContent(
+        "gemini-2.0-flash",
+        [
+            {
+                role: "user",
+                content: prompt,
+            },
+        ],
+        "You are an expert storyteller",
+    );
+
+    const { title, premise } = parseResponse(generatedContent);
+
     return res.status(200).json({
-        adventureTitle: "An adventure title",
-        firstMessage: "Hello from Next.js!",
+        adventureTitle: title,
+        firstMessage: premise,
     });
 }
+
+const generatePrompt = (characterName: string): string => {
+    return `
+<task>
+    Generate an amazing story title and premise based on the character name given. 
+    Your story premise should be interesting and invite further action, asking what they will do next.
+</task>
+<context>
+    <character_name>${characterName}</character_name>
+</context>
+<formatting>
+    - Do not use any markdown formatting. Only use plaintext.
+    - Use the structure shown in <output_example> and do not write anything outside of the <title> and <story_premise> tags.
+</formatting>
+<output_example>
+    <title>Your amazing story title goes here</title>
+    <story_premise>
+        Your amazing story premise goes here
+    </story_premise>
+</output_example>
+    `;
+};
+
+const parseResponse = (
+    response: string,
+): { title: string; premise: string } => {
+    const parser = new XMLParser();
+    const parsed = parser.parse(response);
+
+    let title = parsed.title;
+    let premise = parsed.story_premise;
+
+    return { title, premise };
+};
---
+++ src/utilities/completions.ts
@@ -0,0 +1,51 @@
+import { GoogleGenAI } from "@google/genai";
+import OpenAI from "openai";
+
+type ModelType = "gpt-4o-mini" | "gemini-2.0-flash";
+
+type Context = Array<{
+    role: "user" | "assistant" | "developer";
+    content: string;
+}>;
+
+export const generateContent = async (
+    model: ModelType,
+    prompt: Context,
+    instructions: string,
+): Promise<string> => {
+    let response = "";
+
+    if (model === "gpt-4o-mini") {
+        const apiKey = process.env.OPENAI_API_KEY;
+        const client = new OpenAI({ apiKey });
+
+        const gptResponse = await client.responses.create({
+            model: "gpt-4o-mini",
+            instructions: instructions,
+            input: prompt.map((part) => ({
+                role: part.role,
+                content: part.content,
+            })),
+        });
+
+        response = gptResponse.output_text;
+    } else if (model === "gemini-2.0-flash") {
+        // Gemini
+        const apiKey = process.env.GEMINI_API_KEY!;
+        const client = new GoogleGenAI({ apiKey });
+        const geminiResponse = await client.models.generateContent({
+            model: "gemini-2.0-flash",
+            contents: prompt.map((part) => ({
+                role: part.role,
+                parts: [{ text: part.content }],
+            })),
+            config: {
+                systemInstruction: instructions,
+            },
+        });
+
+        response = geminiResponse.text!;
+    }
+
+    return response;
+};
